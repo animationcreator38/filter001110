dabb07da77ea1dc33833630d65c31d0b20139f4d
animationcreator38 <147336591+animationcreator38@users.noreply.github.com>
2025-03-11 09:41:57 -0700

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -137,10 +137,12 @@ async def pm_text(bot, message):
 @Client.on_callback_query(filters.regex(r"^reffff"))
 async def refercall(bot, query):
     btn = [[
-        InlineKeyboardButton('invite link', url=f'https://telegram.me/share/url?url=https://t.me/{bot.me.username}?start=reff_{query.from_user.id}&text=Hello%21%20Experience%20a%20bot%20that%20offers%20a%20vast%20library%20of%20unlimited%20movies%20and%20series.%20%F0%9F%98%83'),
-        InlineKeyboardButton(f'⏳ {referdb.get_refer_points(query.from_user.id)}', callback_data='ref_point'),
-        InlineKeyboardButton('Back', callback_data='premium_info')
-    ]]
+            InlineKeyboardButton("⭅ Bᴀᴄᴋ", callback_data='start'),
+            InlineKeyboardButton(f'⏳ {referdb.get_refer_points(query.from_user.id)}', callback_data='ref_point'),
+            InlineKeyboardButton('Iɴᴠɪᴛᴇ Nᴏᴡ ⭆', url=f'https://telegram.me/share/url?url=https://t.me/{bot.me.username}?start=reff_{query.from_user.id}&text=Hello%21%20Experience%20a%20bot%20that%20offers%20a%20vast%20library%20of%20unlimited%20movies%20and%20series.%20%F0%9F%98%83')
+         ],[
+            InlineKeyboardButton('🚀 Sᴜʙꜱᴄʀɪᴘᴛɪᴏɴ Pʟᴀɴꜱ 🚀', callback_data="premium_info")
+          ]]
     reply_markup = InlineKeyboardMarkup(btn)
     await bot.edit_message_media(
             query.message.chat.id, 
