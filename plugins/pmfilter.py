4a7fb5fa3e3b080b156105b16a191ab0a51a14fb
animationcreator38 <147336591+animationcreator38@users.noreply.github.com>
2025-03-13 00:29:37 -0700

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2385,24 +2385,11 @@ async def auto_filter(client, msg, spoll=False):
         ])
 
     else:
-        btn = [
-            [
-                InlineKeyboardButton(
-                    text=f"[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}", callback_data=f'{pre}#{file.file_id}'
-                ),
-            ]
-        ]
-        btn.insert(0, 
-            [
-                InlineKeyboardButton(f'Rᴜʟᴇs', 'select'),
-                InlineKeyboardButton("ʟᴀɴɢᴜᴀɢᴇs", callback_data=f"languages#{key}"),
-                InlineKeyboardButton("Sᴇᴀsᴏɴs", callback_data=f"seasons#{key}")
-            ]
-        )
+        btn = []
         btn.insert(0, [
-            InlineKeyboardButton("🎃 𝗛𝗼𝘄 𝗧𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱 𝗠𝗼𝘃𝗶𝗲 🎃", url=await get_tutorial(message.chat.id))
+            InlineKeyboardButton("🎃 𝗛𝗼𝘄 𝗧𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱 🎃", url=await get_tutorial(message.chat.id))
         ])
-
+        
     if offset != "":
         req = message.from_user.id if message.from_user else 0
         try:
